bba4788e50582d876e97c0ccda0fb5d29d93d95a
KhatiRaj <95840982+KhatiRaj@users.noreply.github.com>
2025-05-08 07:42:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -79,7 +79,19 @@ document.addEventListener('DOMContentLoaded', () => {
                 "9.jpg",
                 "10.jpg",
                 "11.jpg",
-                "12.jpg"
+                "12.jpg",
+                "13.jpg",
+                "14.jpg",
+                "15.jpg",
+                "16.jpg",
+                "17.jpg",
+                "18.jpg",
+                "19.jpg",
+                "21.jpg",
+                "22.jpg",
+                "23.jpg",
+                "24.jpg",
+                "25.jpg"
             ]
         },
         {
